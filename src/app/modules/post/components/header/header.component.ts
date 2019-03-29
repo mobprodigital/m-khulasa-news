7908ccfdfdbb1afed2e8d83903b2e039536b3664
aakash819 <aakash.goel462@gmail.com>
2019-03-29 13:53:32 +0530

    update set language function in header component

--- src/app/modules/post/components/header/header.component.ts
+++ src/app/modules/post/components/header/header.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { PostService } from 'src/app/services/post/post.service';
 import { NewsCategoryModel } from 'src/app/model/newsCategory.model';
 import { Router } from '@angular/router';
+import { AppLangServiceService } from 'src/app/services/app-lang-service/app-lang-service.service';
+import { AppLangEnum } from 'src/app/enum/app-lang.enum';
+import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -14,17 +17,21 @@ export class HeaderComponent implements OnInit {
   public translate: string = 'translateX(-250px)';
   public show: boolean = true;
   public langBtn: boolean = true;
+  public liHome: string;
   public lang: string;
 
-  constructor(private postService: PostService, private router: Router) {
+  constructor(private postService: PostService,
+    private router: Router,
+    private appLangService: AppLangServiceService,
+  ) {
     this.postService.getMenuCategories()
       .then(catlist => {
-      this.categoryList = catlist;
-        // setTimeout(() => {
-        //   this.postService.scrollTo()
-        // }, 100);
+        this.categoryList = catlist;
       })
-    this.lang = localStorage.getItem('lang');
+    this.liHome = this.appLangService.selectedAppLang === AppLangEnum.Hindi ? 'होम' : 'home';
+    this.lang = this.appLangService.selectedAppLang === AppLangEnum.Hindi ? 'English' : 'हिंदी';
+
+
   }
   public search() {
     if (this.SearchTerm && this.SearchTerm.trim()) {
@@ -44,20 +51,12 @@ export class HeaderComponent implements OnInit {
   }
 
   public setLanguage() {
-    if (localStorage.getItem('lang') == 'hin') {
-      localStorage.setItem('lang', 'eng');
-      this.router.navigate(['/']);
+    this.appLangService.selectedAppLang = this.appLangService.selectedAppLang === AppLangEnum.English ? AppLangEnum.Hindi : AppLangEnum.English;
+    this.router.navigate(['/']).then(() => {
       setTimeout(() => {
         location.reload();
       }, 100);
-    }
-    else {
-      localStorage.setItem('lang', 'hin');
-      this.router.navigate(['/']);
-      setTimeout(() => {
-        location.reload();
-      }, 100);
-    }
+    });
   }
   ngOnInit() {
   }
