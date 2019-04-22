99c6bf92813ce9a15bdd56f4a668620b2971de41
gyansoft786 <gyan.300@outlook.com>
2019-04-22 14:56:21 +0530

    page reload on lang change resolved

--- src/app/modules/post/components/header/header.component.ts
+++ src/app/modules/post/components/header/header.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { PostService } from 'src/app/services/post/post.service';
 import { NewsCategoryModel } from 'src/app/model/newsCategory.model';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { AppLangServiceService } from 'src/app/services/app-lang-service/app-lang-service.service';
 import { AppLangEnum } from 'src/app/enum/app-lang.enum';
 
@@ -11,7 +11,7 @@ import { AppLangEnum } from 'src/app/enum/app-lang.enum';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  public categoryList: NewsCategoryModel[];
+  public categoryList: NewsCategoryModel[] = [];
   public SearchTerm: string = '';
   public translate: string = 'translateX(-250px)';
   public show: boolean = true;
@@ -19,17 +19,41 @@ export class HeaderComponent implements OnInit {
   public liHome: string;
   public lang: string;
 
+  @ViewChild('nav') nav: ElementRef;
+
   constructor(private postService: PostService,
     private router: Router,
     private appLangService: AppLangServiceService,
   ) {
+    this.getMenuCategories();
+    this.appLangService.langChangedEmitter.subscribe(
+      () => {
+        this.getMenuCategories();
+        this.router.navigateByUrl('/');
+      }
+    );
+
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          this.scrollTo();
+        }, 0);
+      }
+
+    });
+
+  }
+
+  private getMenuCategories() {
     this.postService.getMenuCategories()
       .then(catlist => {
+        this.categoryList.length = 0;
         this.categoryList = catlist;
-      })
+      });
     this.liHome = this.appLangService.selectedAppLang === AppLangEnum.Hindi ? 'होम' : 'home';
     this.lang = this.appLangService.selectedAppLang === AppLangEnum.Hindi ? 'English' : 'हिंदी';
   }
+
   public search() {
     if (this.SearchTerm && this.SearchTerm.trim()) {
       this.router.navigate(['search', this.SearchTerm]);
@@ -39,22 +63,34 @@ export class HeaderComponent implements OnInit {
     if (this.translate == 'translateX(-250px)') {
       this.translate = 'translateX(0px)';
       this.show = false;
-    }
-    else {
-      this.translate = 'translateX(-250px)'
+    } else {
+      this.translate = 'translateX(-250px)';
       this.show = true;
     }
 
   }
 
   public setLanguage() {
-    this.appLangService.selectedAppLang = this.appLangService.selectedAppLang === AppLangEnum.English ? AppLangEnum.Hindi : AppLangEnum.English;
-    this.router.navigate(['/']).then(() => {
-      setTimeout(() => {
-        location.reload();
-      }, 100);
-    });
+    this.appLangService.selectedAppLang =
+      this.appLangService.selectedAppLang === AppLangEnum.English ? AppLangEnum.Hindi : AppLangEnum.English;
   }
   ngOnInit() {
   }
+
+  public scrollTo() {
+    const ulHTML: HTMLUListElement = this.nav.nativeElement;
+    if (!ulHTML) {
+      return;
+    }
+    let activeTab: HTMLElement = ulHTML.querySelector('.nav-link-active');
+    if (!activeTab) {
+      return;
+    }
+    const scrollCount = (activeTab.offsetLeft + (activeTab.clientWidth / 2)) - (ulHTML.clientWidth / 2);
+    ulHTML.scrollTo({
+      left: scrollCount,
+      behavior: "smooth"
+    });
+  }
+
 }
