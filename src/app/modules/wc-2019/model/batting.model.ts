fc0dbc252561b808bb387b2bef08ff8a92b05f5a
aakash819 <aakash.goel462@gmail.com>
2019-08-12 10:31:55 +0530

    add new keys for batting model

--- src/app/modules/wc-2019/model/batting.model.ts
+++ src/app/modules/wc-2019/model/batting.model.ts
@@ -3,9 +3,20 @@ export class battingModel {
     playerId: number;
     playerName: string;
     playerImagePath: string;
-    sRate:number;
+    sRate: number;
     score: number;
     ball: number;
     four: number;
     six: number;
+    scoreboard: string;
+    batsmanoutId: number;
+    bowlingPlayerId: number;
+    catchStumPlayerId: number;
+    playerStatus: string;
+
+    // constructor() {
+    //     this.scoreboard = null
+    // }
+
+
 }
